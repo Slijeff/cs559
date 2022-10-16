97190ac56cbb6900dde9b46bf74d449ff50d2855
Slijeff <jeff83636tx@gmail.com>
2022-10-15 19:05:36 -0500

    feat: add compiled

---
+++ assignment4/index.js
@@ -0,0 +1,708 @@
+"use strict";
+(() => {
+  var __defProp = Object.defineProperty;
+  var __export = (target, all) => {
+    for (var name in all)
+      __defProp(target, name, { get: all[name], enumerable: true });
+  };
+
+  // esm/common.js
+  var EPSILON = 1e-6;
+  var ARRAY_TYPE = typeof Float32Array !== "undefined" ? Float32Array : Array;
+  var RANDOM = Math.random;
+  var degree = Math.PI / 180;
+  if (!Math.hypot)
+    Math.hypot = function() {
+      var y = 0, i = arguments.length;
+      while (i--) {
+        y += arguments[i] * arguments[i];
+      }
+      return Math.sqrt(y);
+    };
+
+  // esm/mat3.js
+  function create() {
+    var out = new ARRAY_TYPE(9);
+    if (ARRAY_TYPE != Float32Array) {
+      out[1] = 0;
+      out[2] = 0;
+      out[3] = 0;
+      out[5] = 0;
+      out[6] = 0;
+      out[7] = 0;
+    }
+    out[0] = 1;
+    out[4] = 1;
+    out[8] = 1;
+    return out;
+  }
+
+  // esm/vec3.js
+  function create2() {
+    var out = new ARRAY_TYPE(3);
+    if (ARRAY_TYPE != Float32Array) {
+      out[0] = 0;
+      out[1] = 0;
+      out[2] = 0;
+    }
+    return out;
+  }
+  function length(a) {
+    var x = a[0];
+    var y = a[1];
+    var z = a[2];
+    return Math.hypot(x, y, z);
+  }
+  function fromValues(x, y, z) {
+    var out = new ARRAY_TYPE(3);
+    out[0] = x;
+    out[1] = y;
+    out[2] = z;
+    return out;
+  }
+  function normalize(out, a) {
+    var x = a[0];
+    var y = a[1];
+    var z = a[2];
+    var len3 = x * x + y * y + z * z;
+    if (len3 > 0) {
+      len3 = 1 / Math.sqrt(len3);
+    }
+    out[0] = a[0] * len3;
+    out[1] = a[1] * len3;
+    out[2] = a[2] * len3;
+    return out;
+  }
+  function dot(a, b) {
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+  }
+  function cross(out, a, b) {
+    var ax = a[0], ay = a[1], az = a[2];
+    var bx = b[0], by = b[1], bz = b[2];
+    out[0] = ay * bz - az * by;
+    out[1] = az * bx - ax * bz;
+    out[2] = ax * by - ay * bx;
+    return out;
+  }
+  var len = length;
+  var forEach = function() {
+    var vec = create2();
+    return function(a, stride, offset, count, fn, arg) {
+      var i, l;
+      if (!stride) {
+        stride = 3;
+      }
+      if (!offset) {
+        offset = 0;
+      }
+      if (count) {
+        l = Math.min(count * stride + offset, a.length);
+      } else {
+        l = a.length;
+      }
+      for (i = offset; i < l; i += stride) {
+        vec[0] = a[i];
+        vec[1] = a[i + 1];
+        vec[2] = a[i + 2];
+        fn(vec, vec, arg);
+        a[i] = vec[0];
+        a[i + 1] = vec[1];
+        a[i + 2] = vec[2];
+      }
+      return a;
+    };
+  }();
+
+  // esm/vec4.js
+  function create3() {
+    var out = new ARRAY_TYPE(4);
+    if (ARRAY_TYPE != Float32Array) {
+      out[0] = 0;
+      out[1] = 0;
+      out[2] = 0;
+      out[3] = 0;
+    }
+    return out;
+  }
+  function normalize2(out, a) {
+    var x = a[0];
+    var y = a[1];
+    var z = a[2];
+    var w = a[3];
+    var len3 = x * x + y * y + z * z + w * w;
+    if (len3 > 0) {
+      len3 = 1 / Math.sqrt(len3);
+    }
+    out[0] = x * len3;
+    out[1] = y * len3;
+    out[2] = z * len3;
+    out[3] = w * len3;
+    return out;
+  }
+  var forEach2 = function() {
+    var vec = create3();
+    return function(a, stride, offset, count, fn, arg) {
+      var i, l;
+      if (!stride) {
+        stride = 4;
+      }
+      if (!offset) {
+        offset = 0;
+      }
+      if (count) {
+        l = Math.min(count * stride + offset, a.length);
+      } else {
+        l = a.length;
+      }
+      for (i = offset; i < l; i += stride) {
+        vec[0] = a[i];
+        vec[1] = a[i + 1];
+        vec[2] = a[i + 2];
+        vec[3] = a[i + 3];
+        fn(vec, vec, arg);
+        a[i] = vec[0];
+        a[i + 1] = vec[1];
+        a[i + 2] = vec[2];
+        a[i + 3] = vec[3];
+      }
+      return a;
+    };
+  }();
+
+  // esm/quat.js
+  function create4() {
+    var out = new ARRAY_TYPE(4);
+    if (ARRAY_TYPE != Float32Array) {
+      out[0] = 0;
+      out[1] = 0;
+      out[2] = 0;
+    }
+    out[3] = 1;
+    return out;
+  }
+  function setAxisAngle(out, axis, rad) {
+    rad = rad * 0.5;
+    var s = Math.sin(rad);
+    out[0] = s * axis[0];
+    out[1] = s * axis[1];
+    out[2] = s * axis[2];
+    out[3] = Math.cos(rad);
+    return out;
+  }
+  function slerp(out, a, b, t) {
+    var ax = a[0], ay = a[1], az = a[2], aw = a[3];
+    var bx = b[0], by = b[1], bz = b[2], bw = b[3];
+    var omega, cosom, sinom, scale0, scale1;
+    cosom = ax * bx + ay * by + az * bz + aw * bw;
+    if (cosom < 0) {
+      cosom = -cosom;
+      bx = -bx;
+      by = -by;
+      bz = -bz;
+      bw = -bw;
+    }
+    if (1 - cosom > EPSILON) {
+      omega = Math.acos(cosom);
+      sinom = Math.sin(omega);
+      scale0 = Math.sin((1 - t) * omega) / sinom;
+      scale1 = Math.sin(t * omega) / sinom;
+    } else {
+      scale0 = 1 - t;
+      scale1 = t;
+    }
+    out[0] = scale0 * ax + scale1 * bx;
+    out[1] = scale0 * ay + scale1 * by;
+    out[2] = scale0 * az + scale1 * bz;
+    out[3] = scale0 * aw + scale1 * bw;
+    return out;
+  }
+  function fromMat3(out, m) {
+    var fTrace = m[0] + m[4] + m[8];
+    var fRoot;
+    if (fTrace > 0) {
+      fRoot = Math.sqrt(fTrace + 1);
+      out[3] = 0.5 * fRoot;
+      fRoot = 0.5 / fRoot;
+      out[0] = (m[5] - m[7]) * fRoot;
+      out[1] = (m[6] - m[2]) * fRoot;
+      out[2] = (m[1] - m[3]) * fRoot;
+    } else {
+      var i = 0;
+      if (m[4] > m[0])
+        i = 1;
+      if (m[8] > m[i * 3 + i])
+        i = 2;
+      var j = (i + 1) % 3;
+      var k = (i + 2) % 3;
+      fRoot = Math.sqrt(m[i * 3 + i] - m[j * 3 + j] - m[k * 3 + k] + 1);
+      out[i] = 0.5 * fRoot;
+      fRoot = 0.5 / fRoot;
+      out[3] = (m[j * 3 + k] - m[k * 3 + j]) * fRoot;
+      out[j] = (m[j * 3 + i] + m[i * 3 + j]) * fRoot;
+      out[k] = (m[k * 3 + i] + m[i * 3 + k]) * fRoot;
+    }
+    return out;
+  }
+  var normalize3 = normalize2;
+  var rotationTo = function() {
+    var tmpvec3 = create2();
+    var xUnitVec3 = fromValues(1, 0, 0);
+    var yUnitVec3 = fromValues(0, 1, 0);
+    return function(out, a, b) {
+      var dot5 = dot(a, b);
+      if (dot5 < -0.999999) {
+        cross(tmpvec3, xUnitVec3, a);
+        if (len(tmpvec3) < 1e-6)
+          cross(tmpvec3, yUnitVec3, a);
+        normalize(tmpvec3, tmpvec3);
+        setAxisAngle(out, tmpvec3, Math.PI);
+        return out;
+      } else if (dot5 > 0.999999) {
+        out[0] = 0;
+        out[1] = 0;
+        out[2] = 0;
+        out[3] = 1;
+        return out;
+      } else {
+        cross(tmpvec3, a, b);
+        out[0] = tmpvec3[0];
+        out[1] = tmpvec3[1];
+        out[2] = tmpvec3[2];
+        out[3] = 1 + dot5;
+        return normalize3(out, out);
+      }
+    };
+  }();
+  var sqlerp = function() {
+    var temp1 = create4();
+    var temp2 = create4();
+    return function(out, a, b, c, d, t) {
+      slerp(temp1, a, d, t);
+      slerp(temp2, b, c, t);
+      slerp(out, temp1, temp2, 2 * t * (1 - t));
+      return out;
+    };
+  }();
+  var setAxes = function() {
+    var matr = create();
+    return function(out, view, right, up) {
+      matr[0] = right[0];
+      matr[3] = right[1];
+      matr[6] = right[2];
+      matr[1] = up[0];
+      matr[4] = up[1];
+      matr[7] = up[2];
+      matr[2] = -view[0];
+      matr[5] = -view[1];
+      matr[8] = -view[2];
+      return normalize3(out, fromMat3(out, matr));
+    };
+  }();
+
+  // esm/vec2.js
+  var vec2_exports = {};
+  __export(vec2_exports, {
+    add: () => add2,
+    angle: () => angle,
+    ceil: () => ceil,
+    clone: () => clone2,
+    copy: () => copy3,
+    create: () => create5,
+    cross: () => cross2,
+    dist: () => dist,
+    distance: () => distance,
+    div: () => div,
+    divide: () => divide,
+    dot: () => dot4,
+    equals: () => equals,
+    exactEquals: () => exactEquals2,
+    floor: () => floor,
+    forEach: () => forEach3,
+    fromValues: () => fromValues3,
+    inverse: () => inverse,
+    len: () => len2,
+    length: () => length4,
+    lerp: () => lerp2,
+    max: () => max,
+    min: () => min,
+    mul: () => mul,
+    multiply: () => multiply,
+    negate: () => negate,
+    normalize: () => normalize4,
+    random: () => random,
+    rotate: () => rotate,
+    round: () => round,
+    scale: () => scale2,
+    scaleAndAdd: () => scaleAndAdd,
+    set: () => set2,
+    sqrDist: () => sqrDist,
+    sqrLen: () => sqrLen,
+    squaredDistance: () => squaredDistance,
+    squaredLength: () => squaredLength3,
+    str: () => str,
+    sub: () => sub,
+    subtract: () => subtract,
+    transformMat2: () => transformMat2,
+    transformMat2d: () => transformMat2d,
+    transformMat3: () => transformMat3,
+    transformMat4: () => transformMat4,
+    zero: () => zero
+  });
+  function create5() {
+    var out = new ARRAY_TYPE(2);
+    if (ARRAY_TYPE != Float32Array) {
+      out[0] = 0;
+      out[1] = 0;
+    }
+    return out;
+  }
+  function clone2(a) {
+    var out = new ARRAY_TYPE(2);
+    out[0] = a[0];
+    out[1] = a[1];
+    return out;
+  }
+  function fromValues3(x, y) {
+    var out = new ARRAY_TYPE(2);
+    out[0] = x;
+    out[1] = y;
+    return out;
+  }
+  function copy3(out, a) {
+    out[0] = a[0];
+    out[1] = a[1];
+    return out;
+  }
+  function set2(out, x, y) {
+    out[0] = x;
+    out[1] = y;
+    return out;
+  }
+  function add2(out, a, b) {
+    out[0] = a[0] + b[0];
+    out[1] = a[1] + b[1];
+    return out;
+  }
+  function subtract(out, a, b) {
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    return out;
+  }
+  function multiply(out, a, b) {
+    out[0] = a[0] * b[0];
+    out[1] = a[1] * b[1];
+    return out;
+  }
+  function divide(out, a, b) {
+    out[0] = a[0] / b[0];
+    out[1] = a[1] / b[1];
+    return out;
+  }
+  function ceil(out, a) {
+    out[0] = Math.ceil(a[0]);
+    out[1] = Math.ceil(a[1]);
+    return out;
+  }
+  function floor(out, a) {
+    out[0] = Math.floor(a[0]);
+    out[1] = Math.floor(a[1]);
+    return out;
+  }
+  function min(out, a, b) {
+    out[0] = Math.min(a[0], b[0]);
+    out[1] = Math.min(a[1], b[1]);
+    return out;
+  }
+  function max(out, a, b) {
+    out[0] = Math.max(a[0], b[0]);
+    out[1] = Math.max(a[1], b[1]);
+    return out;
+  }
+  function round(out, a) {
+    out[0] = Math.round(a[0]);
+    out[1] = Math.round(a[1]);
+    return out;
+  }
+  function scale2(out, a, b) {
+    out[0] = a[0] * b;
+    out[1] = a[1] * b;
+    return out;
+  }
+  function scaleAndAdd(out, a, b, scale3) {
+    out[0] = a[0] + b[0] * scale3;
+    out[1] = a[1] + b[1] * scale3;
+    return out;
+  }
+  function distance(a, b) {
+    var x = b[0] - a[0], y = b[1] - a[1];
+    return Math.hypot(x, y);
+  }
+  function squaredDistance(a, b) {
+    var x = b[0] - a[0], y = b[1] - a[1];
+    return x * x + y * y;
+  }
+  function length4(a) {
+    var x = a[0], y = a[1];
+    return Math.hypot(x, y);
+  }
+  function squaredLength3(a) {
+    var x = a[0], y = a[1];
+    return x * x + y * y;
+  }
+  function negate(out, a) {
+    out[0] = -a[0];
+    out[1] = -a[1];
+    return out;
+  }
+  function inverse(out, a) {
+    out[0] = 1 / a[0];
+    out[1] = 1 / a[1];
+    return out;
+  }
+  function normalize4(out, a) {
+    var x = a[0], y = a[1];
+    var len3 = x * x + y * y;
+    if (len3 > 0) {
+      len3 = 1 / Math.sqrt(len3);
+    }
+    out[0] = a[0] * len3;
+    out[1] = a[1] * len3;
+    return out;
+  }
+  function dot4(a, b) {
+    return a[0] * b[0] + a[1] * b[1];
+  }
+  function cross2(out, a, b) {
+    var z = a[0] * b[1] - a[1] * b[0];
+    out[0] = out[1] = 0;
+    out[2] = z;
+    return out;
+  }
+  function lerp2(out, a, b, t) {
+    var ax = a[0], ay = a[1];
+    out[0] = ax + t * (b[0] - ax);
+    out[1] = ay + t * (b[1] - ay);
+    return out;
+  }
+  function random(out, scale3) {
+    scale3 = scale3 === void 0 ? 1 : scale3;
+    var r = RANDOM() * 2 * Math.PI;
+    out[0] = Math.cos(r) * scale3;
+    out[1] = Math.sin(r) * scale3;
+    return out;
+  }
+  function transformMat2(out, a, m) {
+    var x = a[0], y = a[1];
+    out[0] = m[0] * x + m[2] * y;
+    out[1] = m[1] * x + m[3] * y;
+    return out;
+  }
+  function transformMat2d(out, a, m) {
+    var x = a[0], y = a[1];
+    out[0] = m[0] * x + m[2] * y + m[4];
+    out[1] = m[1] * x + m[3] * y + m[5];
+    return out;
+  }
+  function transformMat3(out, a, m) {
+    var x = a[0], y = a[1];
+    out[0] = m[0] * x + m[3] * y + m[6];
+    out[1] = m[1] * x + m[4] * y + m[7];
+    return out;
+  }
+  function transformMat4(out, a, m) {
+    var x = a[0];
+    var y = a[1];
+    out[0] = m[0] * x + m[4] * y + m[12];
+    out[1] = m[1] * x + m[5] * y + m[13];
+    return out;
+  }
+  function rotate(out, a, b, rad) {
+    var p0 = a[0] - b[0], p1 = a[1] - b[1], sinC = Math.sin(rad), cosC = Math.cos(rad);
+    out[0] = p0 * cosC - p1 * sinC + b[0];
+    out[1] = p0 * sinC + p1 * cosC + b[1];
+    return out;
+  }
+  function angle(a, b) {
+    var x1 = a[0], y1 = a[1], x2 = b[0], y2 = b[1], mag = Math.sqrt((x1 * x1 + y1 * y1) * (x2 * x2 + y2 * y2)), cosine = mag && (x1 * x2 + y1 * y2) / mag;
+    return Math.acos(Math.min(Math.max(cosine, -1), 1));
+  }
+  function zero(out) {
+    out[0] = 0;
+    out[1] = 0;
+    return out;
+  }
+  function str(a) {
+    return "vec2(" + a[0] + ", " + a[1] + ")";
+  }
+  function exactEquals2(a, b) {
+    return a[0] === b[0] && a[1] === b[1];
+  }
+  function equals(a, b) {
+    var a0 = a[0], a1 = a[1];
+    var b0 = b[0], b1 = b[1];
+    return Math.abs(a0 - b0) <= EPSILON * Math.max(1, Math.abs(a0), Math.abs(b0)) && Math.abs(a1 - b1) <= EPSILON * Math.max(1, Math.abs(a1), Math.abs(b1));
+  }
+  var len2 = length4;
+  var sub = subtract;
+  var mul = multiply;
+  var div = divide;
+  var dist = distance;
+  var sqrDist = squaredDistance;
+  var sqrLen = squaredLength3;
+  var forEach3 = function() {
+    var vec = create5();
+    return function(a, stride, offset, count, fn, arg) {
+      var i, l;
+      if (!stride) {
+        stride = 2;
+      }
+      if (!offset) {
+        offset = 0;
+      }
+      if (count) {
+        l = Math.min(count * stride + offset, a.length);
+      } else {
+        l = a.length;
+      }
+      for (i = offset; i < l; i += stride) {
+        vec[0] = a[i];
+        vec[1] = a[i + 1];
+        fn(vec, vec, arg);
+        a[i] = vec[0];
+        a[i + 1] = vec[1];
+      }
+      return a;
+    };
+  }();
+
+  // ../../lib/utils.ts
+  function getCursorPosition(canvas2, event) {
+    const rect = canvas2.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    return { x, y };
+  }
+  function get2dCanvas() {
+    let canvas2 = document.getElementById("myCanvas");
+    let ctx2 = canvas2.getContext("2d");
+    return { canvas: canvas2, ctx: ctx2 };
+  }
+
+  // curve.ts
+  var Curve = class {
+    constructor(canvas2, ctx2) {
+      this.points = [];
+      this.hermit_points = [];
+      this.canvas = canvas2;
+      this.ctx = ctx2;
+      this.counter = 0;
+      this.pbar = document.querySelector("#bar");
+      this.prog = document.querySelector("#progress");
+      this.pbar.style.width = "0%";
+      this.init();
+    }
+    updateAndRender(ev) {
+      const { x, y } = getCursorPosition(this.canvas, ev);
+      this.curr_p = [x, y];
+      if (this.counter % 1 == 0) {
+        this.points.push([x, y]);
+      }
+      this.counter = (this.counter + 1) % 100;
+      this.renderCrude();
+    }
+    hermit_basis(t, P) {
+      const basis = [
+        2 * t * t * t - 3 * t * t + 1,
+        t * t * t - 2 * t * t + t,
+        -2 * t * t * t + 3 * t * t,
+        t * t * t - t * t
+      ];
+      let result = vec2_exports.create();
+      vec2_exports.scale(result, P[0], basis[0]);
+      vec2_exports.scaleAndAdd(result, result, P[1], basis[1]);
+      vec2_exports.scaleAndAdd(result, result, P[2], basis[2]);
+      vec2_exports.scaleAndAdd(result, result, P[3], basis[3]);
+      return result;
+    }
+    init() {
+      this.canvas.addEventListener("mouseout", () => {
+        if (this.isMouseDown) {
+          this.isMouseDown = false;
+          this.ctx.beginPath();
+          this.calculateCurve();
+          this.renderHermit();
+        }
+      });
+      this.canvas.addEventListener("mousedown", () => {
+        this.isMouseDown = true;
+        this.points = [];
+        this.hermit_points = [];
+      });
+      this.canvas.addEventListener("mouseup", () => {
+        this.isMouseDown = false;
+        this.ctx.beginPath();
+        this.calculateCurve();
+        this.renderHermit();
+      });
+      this.canvas.addEventListener("mousemove", (ev) => {
+        if (this.isMouseDown) {
+          this.updateAndRender(ev);
+        }
+      });
+      const clear_btn = document.querySelector("#clear");
+      clear_btn.addEventListener("click", () => {
+        this.clear();
+      });
+    }
+    calculateCurve() {
+      console.log("sample length: ", this.points.length);
+      for (let i = 0; i < this.points.length - 2; i++) {
+        let p1 = this.points[i];
+        let p2 = this.points[i + 1];
+        let p3 = this.points[i + 2];
+        let d1 = [p2[0] - p1[0], p2[1] - p1[1]];
+        let d2 = [p3[0] - p2[0], p3[1] - p2[1]];
+        this.hermit_points.push([p1, d1, p2, d2]);
+      }
+      console.log("hermit length: ", this.hermit_points.length);
+    }
+    clear() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.points = [];
+      this.hermit_points = [];
+    }
+    renderHermit() {
+      this.ctx.strokeStyle = "rgba(23,215,232,0.89)";
+      this.ctx.lineWidth = 5;
+      let render_p = [];
+      for (let i = 0; i < this.hermit_points.length; i++) {
+        for (let t = 0; t <= 1; t += 0.2) {
+          if (this.hermit_points[i]) {
+            this.prog.innerText = `${i}`;
+            let res = this.hermit_basis(t, this.hermit_points[i]);
+            render_p.push([res[0], res[1]]);
+            this.pbar.style.width = `${i / this.hermit_points.length * 100}%`;
+          }
+        }
+      }
+      for (const p of render_p) {
+        this.ctx.lineTo(p[0], p[1]);
+      }
+      this.ctx.stroke();
+      this.ctx.beginPath();
+      console.log("render length: ", render_p.length);
+    }
+    renderCrude() {
+      this.ctx.strokeStyle = "black";
+      this.ctx.lineWidth = 1;
+      this.ctx.lineTo(this.curr_p[0], this.curr_p[1]);
+      this.ctx.stroke();
+    }
+  };
+
+  // index.ts
+  var { canvas, ctx } = get2dCanvas();
+  var hermite_curve = new Curve(canvas, ctx);
+  console.log(hermite_curve);
+})();
+//# sourceMappingURL=index.js.map
